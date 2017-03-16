c2d42319fa8552fc757deae32770fccf6980d507
negebauer <negebauer@uc.cl>
2017-03-16 18:48:29 -0300

    Improve courses parsing code

--- portal.js
+++ portal.js
@@ -1,67 +1,57 @@
-const cheerio = require('cheerio');
-const iconv = require('iconv-lite');
-const request = require('./request');
-const urls = require('./urls');
-const course = require('./course');
+const cheerio = require('cheerio')
+const iconv = require('iconv-lite')
+const request = require('./request')
+const urls = require('./urls')
+const course = require('./course')
 
 class Portal {
   static coursesList(session, ignore = []) {
     return session.login().then(() => {
-      console.log('Getting courses list');
+      console.log('Getting courses list')
       return new Promise((res, rej) => {
         request({url: urls.portal, encoding: null}, (err, http, body) => {
           if (err) {
-            rej(err);
+            rej(err)
           }
-          console.log('Parsing courses list');
-          const regex = /\d+-\d+-([a-z]+\d+)-\d ([ ,\ta-zA-Zá-úÁ-Ú]+)/g;
-          const empty = {link: '', name: '', acronym: ''};
-          const $ = cheerio.load(iconv.decode(body, 'ISO-8859-1'));
-          const data1 = $('ul#siteLinkList')
+          console.log('Parsing courses list')
+          const regex = /(\d+)-(\d+)-([a-z]+\d+)-(\d) ([ ,a-zA-Zá-úÁ-Ú]+)/g
+          const coursesData = []
+          const courseData = (i, l) => {
+            const name = $(l).text()
+            const nameRegex = regex.exec(name)
+            if (!nameRegex) {
+              return
+            }
+            const courseData = {
+              year: nameRegex[1],
+              semester: nameRegex[2],
+              acronym: nameRegex[3].toUpperCase(),
+              section: nameRegex[4],
+              name: nameRegex[5],
+            }
+            coursesData.push(courseData)
+          }
+          const dataFilter = k => Number(k) >= 0
+          const ignoreFilter = c => ignore.indexOf(c.acronym) === -1
+          const courseObject = d => new course(d)
+          const $ = cheerio.load(iconv.decode(body, 'ISO-8859-1'))
+          const coursesUpperBar = $('ul#siteLinkList')
             .find('li')
             .find('a')
-            .map((i, l) => {
-              const link = $(l).attr('href');
-              const linkName = $(l).text();
-              const linkNameRegex = regex.exec(linkName);
-              if (!linkNameRegex) {
-                return empty;
-              }
-              const acronym = linkNameRegex[1].toUpperCase();
-              const name = linkNameRegex[2];
-              return {link, name, acronym};
-            });
-          const array1 = Object.keys(data1)
-            .filter(k => Number(k) >= 0)
-            .map(k => data1[k]);
-          const data2 = $('div#selectNav')
+            .each(courseData)
+          const coursesMore = $('div#selectNav')
             .find('select')
             .find('option')
-            .map((i, l) => {
-              const link = $(l).val();
-              const linkName = $(l).text();
-              const linkNameRegex = regex.exec(linkName);
-              if (!linkNameRegex) {
-                return empty;
-              }
-              const acronym = linkNameRegex[1].toUpperCase();
-              const name = linkNameRegex[2];
-              return {link, name, acronym};
-            });
-          const array2 = Object.keys(data2)
-            .filter(k => Number(k) >= 0)
-            .map(k => data2[k]);
-          const courses = []
-            .concat(array1, array2)
-            .filter(i => i.link)
-            .filter(i => ignore.indexOf(i.acronym) === -1)
-            .map(d => new course(d.acronym, d.name, d.link));
-          console.log('courses');
-          console.log(courses);
-        });
-      });
-    });
+            .each(courseData)
+            .filter(dataFilter)
+          const courses = coursesData.filter(ignoreFilter).map(courseObject)
+          console.log('courses')
+          console.log(courses)
+          res(courses)
+        })
+      })
+    })
   }
 }
 
-module.exports = Portal;
+module.exports = Portal
